037bb9d8b69c4873f1dd4eea9ae8e598d31f88b5
Morris <morrisho215215@gmail.com>
2024-03-05 09:50:11 +0800

    Reduce backend redundancy

--- backend/controllers/goalcontroller.js
+++ backend/controllers/goalcontroller.js
@@ -46,15 +46,13 @@ const updateGoal = asyncHandler( async (req, res) => {
     throw new Error("Goal not found")
   }
 
-  const user = await User.findById(req.user.id)
-
-  if (!user){
+  if (!req.user){
     res.status(401)
     throw new Error("User not found")
   }
 
   //make sure login user matches to goal user
-  if (goal.user.toString()!= user.id){
+  if (goal.user.toString()!= req.user.id){
     res.status(401)
     throw new Error("User not authorised")
   }
@@ -75,15 +73,14 @@ const deleteGoal = asyncHandler( async (req, res) => {
     throw new Error("Goal not found")
   }
 
-  const user = await User.findById(req.user.id)
 
-  if (!user){
+  if (!req.user){
     res.status(401)
     throw new Error("User not found")
   }
 
   //make sure login user matches to goal user
-  if (goal.user.toString()!= user.id){
+  if (goal.user.toString()!= req.user.id){
     res.status(401)
     throw new Error("User not authorised")
   }
